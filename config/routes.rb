09412ae29fa489eeef20a24f653246ab225f5213
Shane <shane.boyar@gmail.com>
2019-09-09 09:40:03 -0400

    clear repo

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :teams
-  devise_for :users
-  get 'pages/root'
-
-  root to: "pages#root"
-end
--- db/migrate/20190909123702_add_columns_to_teams.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToTeams < ActiveRecord::Migration[5.1]
-  def change
-    add_column :teams, :abbreviation, :string
-    add_column :teams, :conference, :string
-    add_column :teams, :division, :string
-  end
-end
